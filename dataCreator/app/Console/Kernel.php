5a05d8e2c779018c1736b439b8c90ab10a31a522
Martin <martinfifa2@gmail.com>
2023-01-17 12:24:11 +0100

    working

--- dataCreator/app/Console/Kernel.php
+++ dataCreator/app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command(CreateStock::class)->weekdays()->everyMinute()->timezone('Europe/Madrid');//->between('9:00','17:00');
+        $schedule->command(CreateStock::class)->weekdays()->everyMinute()->timezone('Europe/Madrid')->between('9:00','17:00');
     }
     
     protected function scheduleTimezone()
